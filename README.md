e0b0f4ba32032325f22a33c25e247cb75fb15b6a
Vadim <vadimgoncharuk95@gmail.com>
2020-05-12 23:20:16 -0700

    Updated Documentation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # JavaTurnStratGame
+A turn-based strategy game build in Java using Swing and AWT. The game is rendered on a Java AWT canvas and is updated 60 times a second on a thread separate from the main window thread. Images are loaded from Sheet.png Spritesheet and the map is generated from a PNG where each pixel color represents a particular terrain.
+
+Inspired by Sid Meier's Civilization game series. In this game, the user can move the camera around with the arrow keys to pan the map, hover the mouse over tiles to see details as the tile, select a unit and move them once per turn, go to next turn and build huts with the settler unit.
+
+![Game1](./Screenshots/Image1.png)
+
+![Game2](./Screenshots/Image2.png)
+
+The game is still in its earlier iteration. New planned features include more units such as ships, more terrain tiles such as deserts, more structures to build, resources to gather, allow saving and loading maps, AI to play against, and sounds.
--- src/Engine/GridMap.java
+++ src/Engine/GridMap.java
@@ -2,7 +2,6 @@ package Engine;
 import java.awt.Color;
 import java.awt.Graphics;
 
-import Art.Art;
 import Art.Tile;
 
 public class GridMap {
@@ -25,13 +24,6 @@ public class GridMap {
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid[0].length; j++) {
 				g.drawImage(Tile.TILE_MAP.get(grid[i][j]).getImage(), j * (GRID_WIDTH) - c.getX(), i * (GRID_WIDTH) - c.getY(), GRID_WIDTH, GRID_HEIGHT, null);
-
-				//				if (grid[i][j] == 0) {//Art.COLORMAP.get(new Color(0,255,0))
-				//					g.drawImage(Art.SPRITE[2][0], j * (GRID_WIDTH) - c.getX(), i * (GRID_WIDTH) - c.getY(), GRID_WIDTH, GRID_HEIGHT, null);
-				//				}
-				//				if (grid[i][j] == 1) {
-				//					g.drawImage(Art.SPRITE[2][2], j * (GRID_WIDTH) - c.getX(), i * (GRID_WIDTH) - c.getY(), GRID_WIDTH, GRID_HEIGHT, null);
-				//				}
 				g.drawRect(j * GRID_WIDTH - c.getX(), i * GRID_HEIGHT - c.getY(), GRID_WIDTH, GRID_HEIGHT);
 			}
 		}
@@ -40,16 +32,4 @@ public class GridMap {
 	public void tick () {}
 
 	public void click(int x, int y, Camera c) {	}
-
-
-	//	private void printInts(int... args) {
-	//		for (int i = 0; i < args.length; i++) {
-	//			System.out.print(args[i]);
-	//			if (i < args.length) {
-	//				System.out.print(", ");
-	//			}
-	//		}
-	//		System.out.print("\n");
-	//	}
-
 }
